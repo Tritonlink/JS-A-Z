5d275be79ecb71efb2dfe9c743741e3c14bc26a9
Tritonlink <marieoulabou@gmail.com>
2021-09-15 22:24:28 +0200

    calculatrice

--- App.js
+++ App.js
@@ -1,2 +1,30 @@
-const va = 55;
-.console.log(va);
+let total = 0;
+
+function addition(x, y) {
+  total = x + y;
+  return total;
+}
+function add(x) {
+  total += x;
+  return total;
+}
+function addmultiply(x) {
+  total *= x;
+  return total;
+}
+function multiplication(x, y) {
+  total = x * y;
+  return total;
+}
+function adddiv(x) {
+  total /= x;
+  return total;
+}
+function div(x, y) {
+  total = x / y;
+  return total;
+}
+function clear() {
+  total = 0;
+  return total;
+}
