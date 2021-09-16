9d5fcba6bf1685245cac041436d6bbbadbe69d1a
Tritonlink <marieoulabou@gmail.com>
2021-09-16 22:33:57 +0200

    Dom-manipulation-1h30

--- App.js
+++ App.js
@@ -1,30 +1,48 @@
-let total = 0;
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const answer = document.querySelector("p");
+const showAnswer = document.getElementsByClassName("show-answer");
+const mousemove = document.querySelector(".mousemove");
+const keypressContainer = document.querySelector(".keypress");
+const key = document.getElementById("key");
 
-function addition(x, y) {
-  total = x + y;
-  return total;
-}
-function add(x) {
-  total += x;
-  return total;
-}
-function addmultiply(x) {
-  total *= x;
-  return total;
-}
-function multiplication(x, y) {
-  total = x * y;
-  return total;
-}
-function adddiv(x) {
-  total /= x;
-  return total;
-}
-function div(x, y) {
-  total = x / y;
-  return total;
-}
-function clear() {
-  total = 0;
-  return total;
-}
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+btn1.addEventListener("click", () => {
+  answer.classList.add("show-answer");
+  answer.style.background = "#41b441";
+});
+btn2.addEventListener("click", () => {
+  answer.classList.add("show-answer");
+  answer.style.background = "#bb3c3c";
+});
+
+window.addEventListener("mousemove", (e) => {
+  mousemove.style.left = e.pageX + "px";
+  mousemove.style.top = e.pageY + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+  mousemove.style.border = "2px solid pink";
+});
+window.addEventListener("mouseup", () => {
+  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
+  mousemove.style.border = "2px solid teal";
+});
+
+questionContainer.addEventListener("mouseenter", () => {
+  questionContainer.style.background = "rgba(0,0,0,0.6) ";
+});
+questionContainer.addEventListener("mouseout", () => {
+  questionContainer.style.background = "rgba(0,0,0)";
+});
+answer.addEventListener("mouseover", () => {
+  answer.style.transform = "rotate(5deg) ";
+});
+
+document.addEventListener("keypress", (e) => {
+  key.textContent = e.key;
+});
