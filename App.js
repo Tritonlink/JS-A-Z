759809c28124da32c7f6a33452233978bf2ae921
Tritonlink <marieoulabou@gmail.com>
2021-09-17 02:36:09 +0200

    Dom-manipulation2h27

--- App.js
+++ App.js
@@ -6,6 +6,18 @@ const showAnswer = document.getElementsByClassName("show-answer");
 const mousemove = document.querySelector(".mousemove");
 const keypressContainer = document.querySelector(".keypress");
 const key = document.getElementById("key");
+const nav = document.querySelector("nav");
+const inputName = document.querySelector("input[type= text]");
+const select = document.querySelector("select");
+const form = document.querySelector("form");
+let language;
+let pseudo;
+const ring = () => {
+  const audio = new Audio();
+  audio.src = "./Enter.mp3";
+  audio.play();
+};
+console.log();
 
 questionContainer.addEventListener("click", () => {
   questionContainer.classList.toggle("question-clicked");
@@ -45,4 +57,35 @@ answer.addEventListener("mouseover", () => {
 
 document.addEventListener("keypress", (e) => {
   key.textContent = e.key;
+  ring();
+});
+
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 120) {
+    nav.style.top = 0;
+  } else {
+    nav.style.top = -50 + "px";
+  }
+});
+
+inputName.addEventListener("input", (e) => {
+  pseudo = e.target.value;
+  console.log(pseudo);
+});
+
+select.addEventListener("input", (e) => {
+  language = e.target.value;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (cgv.checked === true) {
+    document.querySelector("form > div").innerHTML = `
+    <h3> Pseudo : ${pseudo} </h3>
+    <h4> langage préféré : ${language} </h4>
+    `;
+  } else {
+    alert("Veuillez acceptez les CGV !");
+  }
 });
